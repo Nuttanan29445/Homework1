beedf44b210060f13b1680838df67773ef15395e
NUTTANAN RUANGPHANICH <63010320@KMITL.AC.TH>
2020-08-15 13:15:43 +0700

    Add Grade +

--- Homework1/Source.cpp
+++ Homework1/Source.cpp
@@ -7,21 +7,33 @@ int main() {
 	{
 		printf("Your grade:F\n");
 	}
-	else if (score >= 50 && score <= 59)
+	else if (score >= 50 && score <= 54)
 	{
 		printf("Your grade:D\n");
 	}
-	else if (score >= 60 && score <= 69)
+	else if (score >= 55 && score <= 59)
 	{
-		printf("Your grade:C\n");
+		printf("Your grade:D+\n");
 	}
-	else if (score >= 70 && score <=79)
+	else if (score >= 60 && score <=64)
 	{
-		printf("Your grade:B");
+		printf("Your grade:C");
+	}
+	else if (score >= 65 && score <= 69)
+	{
+		printf("Your grade:C+");
+	}
+	else if (score >= 70 && score <= 74)
+	{
+	printf("Your grade:B\n");
+	}
+	else if (score >= 75 && score <= 79)
+	{
+	printf("Your grade:B+\n");
 	}
 	else
 	{
-		printf("Your grade:A");
+	printf("Your grade:A");
 	}
 	return 0;
 }
